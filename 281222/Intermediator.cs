3ff3bd749ffe266d4cf1a655561dd1ac53e5f474
Iftime Vasile Marian <PC-i7@DESKTOP-OH8TSUB>
2022-12-28 16:44:16 +0200

    Commit and push

--- 281222/Intermediator.cs
+++ 281222/Intermediator.cs
@@ -108,9 +108,10 @@ namespace _281222
             Component1 component1 = new Component1();
             Component2 component2 = new Component2();
             new ConcreteMediator(component1, component2);
-
+            ///complicata treaba
             Console.WriteLine("Client triggets operation A.");
             component1.DoA();
+            
 
 
 
